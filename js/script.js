b24d75ae7ae3ab69fbf06af6f401144cdd01d318
Kelvin Yeboah <onlykelvin06@gmail.com>
2025-03-22 16:20:39 -0700

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,10 @@
+function goToScanner() {
+    window.location.href = "pages/scanner.html";
+}
 
+function toggleDarkMode() {
+    document.body.classList.toggle("dark-mode");
+    const icon = document.getElementById("dark-mode-icon");
+    icon.classList.toggle("fa-moon");
+    icon.classList.toggle("fa-sun");
+}
