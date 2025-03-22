e169bc37d6954e0256bd9a857840717d7bbfd1a2
Kelvin Yeboah <onlykelvin06@gmail.com>
2025-03-22 16:20:26 -0700

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,7 +1,1 @@
-fultDisplay.innerHTML = "🚨 Warning! This site is flagged as dangerous!";
-        resultDisplay.style.color = "red";
-    } else {
-        resultDisplay.innerHTML = "✅ Safe! No threats detected.";
-        resultDisplay.style.color = "green";
-    }
-}
+
