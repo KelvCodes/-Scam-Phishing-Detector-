b871d8048d9bea9ab6333319be8aefd90eb73629
Kelvin Yeboah <onlykelvin06@gmail.com>
2025-03-22 16:19:38 -0700

    Update scanner.js

--- js/scanner.js
+++ js/scanner.js
@@ -1,7 +1,1 @@
-    resultText.innerHTML = "⚠️ This website is suspicious!";
-        resultText.style.color = "red";
-    } else {
-        resultText.innerHTML = "✅ This website looks safe.";
-        resultText.style.color = "green";
-    }
-}
+
