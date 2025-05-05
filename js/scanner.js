ad5a0164f232d40c2eb367e31628ae6eed59aed4
Kelvin Yeboah <onlykelvin06@gmail.com>
2025-05-05 14:53:29 -0700

    Update scanner.js

--- js/scanner.js
+++ js/scanner.js
@@ -1,4 +1,4 @@
- (!urlPattern.test(urlInput)) {
+ {
         resultText.innerHTML = '<i class="fas fa-times-circle"></i> Invalid URL format!';
         resultText.style.color = "#ff4444";
     } else if (isSuspicious) {
