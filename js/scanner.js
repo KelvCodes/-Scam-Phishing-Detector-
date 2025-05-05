d568057b68f03a25f3fd8ca119a7bd7594e0b52e
Kelvin Yeboah <onlykelvin06@gmail.com>
2025-05-05 14:32:54 -0700

    Update scanner.js

--- js/scanner.js
+++ js/scanner.js
@@ -1,4 +1,4 @@
-cam() {
+{
     const urlInput = document.getElementById("url-input").value.trim();
     const resultText = document.getElementById("result");
     const loading = document.getElementById("loading");
